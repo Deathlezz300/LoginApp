cecec98f4524ccaa4d8fb7f6ce20ab35e2e933fa
Alejandro <jairoalejandro2003@gmail.com>
2023-02-08 08:49:13 -0500

    another commit

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -1,8 +1,27 @@
 import { useState } from "react"
+import { useEffect } from "react";
+import { useMemo } from "react";
 
-export const useForm=(initialState)=>{
+export const useForm=(initialState,formValidations={})=>{
 
     const [form, setform] = useState(initialState);
+    const [formValidation, setformValidation] = useState({});
+
+    useEffect(() => {
+        createValidators();      
+    }, [form])
+    
+    const isFormValid=useMemo(()=>{
+
+        for (const formValue of Object.keys(formValidation)) {
+            if(formValidation[formValue]!=null){
+                return false
+            }
+
+            return true
+        }
+
+    } , [formValidation]);
 
     const onInputChange=({target})=>{
         const {name,value}=target;
@@ -16,10 +35,25 @@ export const useForm=(initialState)=>{
         setform(initialState);
     }
 
+    const createValidators=()=>{
+
+        const formCheckedValues={};
+
+        for (const formField of Object.keys(formValidations)) {
+            const [fnValidation,errorMessage]=formValidations[formField];
+
+            formCheckedValues[`${formField}Valid`]=fnValidation(form[formField]) ? null :errorMessage;
+        }
+
+        setformValidation(formCheckedValues);
+    };
+
     return {
         form,
         onInputChange,
-        onResetForm
+        onResetForm,
+        formValidation,
+        isFormValid
     }
 
 }
--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,30 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Grid, Link, TextField } from '@mui/material'
 import { Typography } from '@mui/material'
 import { Google } from '@mui/icons-material'
 import { Link as RouterLink } from 'react-router-dom'
 import { AuthLayout } from './AuthLayout'
+import { useForm } from '../../Hooks/useForm'
+
+const formValidations={
+  email:[(value)=>value.includes('@'),'No es una direccion de correo'],
+  password:[(value)=>value.length>=6,'La clave debe tener mas de 6 caracteres'],
+  name:[(value)=>value.length>=1,'El nombre es obligatorio']
+}
 
 export const RegisterPage = () => {
+
+  const [formSubmit, setFormSubmit] = useState(false);
+
+  const {form,onInputChange,formValidation,isFormValid}=useForm({name:'',email:'',password:''},formValidations)
+
+  const handleSubmit=(evento)=>{
+    evento.preventDefault();
+    setFormSubmit(true);
+  }
+
   return (
       <AuthLayout titulo='Registro'>
 
-      <form>
+      <form onSubmit={handleSubmit}>
           <Grid container>
             <Grid item xs={12} sx={{paddingBottom:2}}>
-              <TextField label="Nombre completo" type='text' fullWidth></TextField>
+              <TextField error={!!formValidation.nameValid && formSubmit} helperText={formValidation.nameValid}  value={form.name} name='name' onChange={onInputChange} label="Nombre completo" type='text' fullWidth></TextField>
             </Grid>
             <Grid item xs={12} sx={{paddingBottom:2}}>
-              <TextField label="Correo" type='email' fullWidth></TextField>
+              <TextField type='email' error={!!formValidation.emailValid && formSubmit} helperText={formValidation.emailValid}  value={form.email} name='email' onChange={onInputChange} label="Correo"  fullWidth></TextField>
             </Grid>
             <Grid item xs={12}>
-              <TextField label="Contraseña" type='password' fullWidth></TextField>
+              <TextField error={!!formValidation.passwordValid && formSubmit} helperText={formValidation.passwordValid}  value={form.password} name='password' onChange={onInputChange} label="Contraseña" type='password' fullWidth></TextField>
             </Grid>
           </Grid>
 
           <Grid container spacing={2} sx={{paddingTop:1.5}}>
             <Grid item xs={12} >
-              <Button variant='contained' fullWidth>Crear cuenta</Button>                
+              <Button type='submit' variant='contained' fullWidth>Crear cuenta</Button>                
             </Grid>
             <Grid container direction='row' justifyContent='end' sx={{paddingTop:1}}>
                 <Typography sx={{mr:1}}>¿Ya tienes una cuenta?</Typography>
